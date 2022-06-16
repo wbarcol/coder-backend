9352dff11af774f8d1e9626a53c2807ced6f9b6e
Wil <wbarcol@gmail.com>
2022-06-16 20:27:25 -0300

    4ta entrega

--- index.js
+++ index.js
@@ -1,72 +1,19 @@
-const fs = require('fs')
 const express = require('express')
 const app = express()
-const puerto = 8080
+const rutas = require('./routes/index')
 
 
-class Contenedor {
-    constructor (archivo){
-        this.archivo = archivo;
-    }
-    
-//    Entrega un producto random
-
-    async getById() {
-        try{
-            let data = await fs.promises.readFile(`./${this.archivo}.txt`, 'utf-8')
-            let allData = JSON.parse(data)
-
-            app.get('/productoRandom', (req, res) =>{
-                let maxRandom = allData.length
-                let numRandom = Math.floor(Math.random() * maxRandom) 
-                let productoRandom = allData[numRandom]
-                res.send(`<h3 style="color: red">Producto: ${productoRandom.title} precio: ${productoRandom.price} </h3>`)
-            })
-
-            
-        } catch (error) {
-            console.log (`Verificar hubo un error ${error}`)
-        }
-    }
-
-    //    Devuelve un array con los objetos presentes en el archivo
-
-    async getAll() {
-        try{
-            let data = await fs.promises.readFile(`./${this.archivo}.txt`, 'utf-8')
-            let allData = JSON.parse(data)
-
-            app.get('/productos', (req, res) =>{
-                res.json(allData.map(product =>
-                    `${product.title}`
-                  ).join(','))
-            })
-
-        } catch (error) {
-            console.log (`Verificar hubo un error ${error}`)
-        }
-
-    }
-
-}
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
 
 
-const compra = new Contenedor('productos')
-compra.getAll()
-compra.getById()
+app.use('/api', rutas)
 
-
-
-app.get('/', (req, res) =>{
-    res.send('<h1 style="color: blue">Bienvenido al servidor</h1>')
+app.use((error, req, res, next) =>{
+    res.status(500).send(error)
 })
 
-
-app.listen(puerto, (error) =>{
-
-    if (!error) {
-    console.log(`el servidor se inicio en el puerto ${puerto}`)
-    } else {
-        console.log('hubo un error: ', error)
-    }
+app.listen(8080, () => {
+    console.log('Servidor escuchando el puerto 8080')
 })
